b16f920e333d678ba04ff9659ffd41e11e69334d
JacquesEngelbrecht <jacquesjengelbrecht@gmail.com>
2020-12-15 07:27:24 +0200

    Added all sockets

--- public/script.js
+++ public/script.js
@@ -12,12 +12,115 @@ document.addEventListener('DOMContentLoaded', () => {
     const rotateButton = document.getElementById('rotate')
     const turnDisplay = document.getElementById('whose-go')
     const infoDisplay = document.getElementById('info')
+    const singlePlayerButton = document.querySelector('#singlePlayerButton')
+    const multiPlayerButton = document.querySelector('#multiPlayerButton')
     const userSquares = []
     const computerSquares = []
     const width = 10
     let isHorizontal = true
     let isgameover = false
     let currentPlayer = 'user' 
+    let gameMode = ""
+    let playerNum = 0
+    let ready = false
+    let enemyReady = false
+    let allShipsPlaced = false
+    let shotFired = -1
+
+    //Select mode
+    singlePlayerButton.addEventListener('click', startSinglePlayer)
+    multiPlayerButton.addEventListener('click', startMultiPlayer) 
+
+    //Single Player
+    function startSinglePlayer() {
+        gameMode = 'singlePlayer'
+
+        generate(shipArray[0])
+        generate(shipArray[1])
+        generate(shipArray[2])
+        generate(shipArray[3])
+        generate(shipArray[4])
+        startButton.addEventListener('click', playGameSingle)
+    }
+
+    //Multi Player
+    function startMultiPlayer() {
+        gameMode = 'multiPlayer'
+        const socket = io()
+
+         //Get player number
+        socket.on('player-number', num => {
+            if(num === -1) {
+                infoDisplay.innerHTML = 'Sorry the server is full!'
+            } else {
+                playerNum = parseInt(num)
+                if(playerNum === 1) currentPlayer = "enemy"
+                console.log(playerNum)
+
+                //Get other player status
+                socket.emit('check-players')
+            }
+        })
+        //Antohter player connected or disconnected
+        socket.on('player-connection', num => {
+            console.log(`Player number ${num} has connected or disconnected`)
+            playerConnectedOrDisconnected(num)
+        })
+
+        //On enemy ready
+        socket.on('enemy-ready', num => {
+            enemyReady = true
+            playerReady(num)
+            if (ready) playGameMulti(socket)
+        })
+
+        //Check player status
+        socket.on('check-players', players => {
+            players.forEach((p, i) => {
+                if(p.connected) playerConnectedOrDisconnected(i)
+                if(p.ready) {
+                    playerReady(i)
+                    if(i !== playerNum) enemyReady = true
+                }
+            })
+        })
+
+        //Ready button event
+        startButton.addEventListener('click', () => {
+            if(allShipsPlaced) playGameMulti(socket)
+            else infoDisplay.innerHTML = 'Please place all of your ships!'
+        })
+
+        //Setup listener for firing
+        computerSquares.forEach(square => {
+            square.addEventListener('click' ,() => {
+                if(currentPlayer === 'user' && ready && enemyReady) {
+                    shotFired = square.dataset.id 
+                    socket.emit('fire', shotFired)
+                }
+            })
+        })
+
+        //On fire received
+        socket.on('fire', id => {
+            enemyGo(id)
+            const square = userSquares[id]
+            socket.emit('fire-reply', square.classList)
+            playGameMulti(socket)
+        })
+
+        //On fire reply received
+        socket.on('fire-reply', classList => {
+            revealSquare(classList)
+            playGameMulti(socket)
+        })
+
+        function playerConnectedOrDisconnected(num) {
+            let player = `.p${parseInt(num) + 1}`
+            document.querySelector(`${player} .connected span`).classList.toggle('green')
+            if(parseInt(num) === playerNum) document.querySelector(player).style.fontWeight = 'bold'
+        }
+    }
 
     //Create boards
     function createBoard(grid, squares) {
@@ -70,29 +173,25 @@ document.addEventListener('DOMContentLoaded', () => {
             ]
         }
     ]
-    //Computer's random board
+    //Computer's square board
     function generate(ship) {
-        let randomDirection = Math.floor(Math.random() * ship.directions.length)
-        let current = ship.directions[randomDirection]
-        if (randomDirection === 0) direction = 1
-        if (randomDirection === 1) direction = 10
-        let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
+        let squareDirection = Math.floor(Math.square() * ship.directions.length)
+        let current = ship.directions[squareDirection]
+        if (squareDirection === 0) direction = 1
+        if (squareDirection === 1) direction = 10
+        let squareStart = Math.abs(Math.floor(Math.square() * computerSquares.length - (ship.directions[0].length * direction)))
             
-        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
-        const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
-        const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+        const isTaken = current.some(index => computerSquares[squareStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(index => (squareStart + index) % width === width - 1)
+        const isAtLeftEdge = current.some(index => (squareStart + index) % width === 0)
 
-        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[squareStart + index].classList.add('taken', ship.name))
 
         else generate(ship)
   
     }
 
-    generate(shipArray[0])
-    generate(shipArray[1])
-    generate(shipArray[2])
-    generate(shipArray[3])
-    generate(shipArray[4])
+    
 
     //Rotate
     function rotate() {
@@ -161,7 +260,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex)
         selectedShipIndex = parseInt(selectedShipnameWithIndex.substr(-1))
         shipLastId = shipLastId - selectedShipIndex
-        //console.log(shipLastId)
+
         if(isHorizontal  && !newNotAllowedHorizontal.includes(shipLastId)) {
             for(let i = 0; i < draggedShipLength; i++) {
                userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass)
@@ -173,14 +272,38 @@ document.addEventListener('DOMContentLoaded', () => {
         } else return
 
         displayGrid.removeChild(draggedShip)
+        if(!displayGrid.querySelector('.ship')) allShipsPlaced = true
     }
 
     function dragEnd() {
         console.log('dragEnd')
     }
 
-    //Games logic
-    function playGame() {
+    //Game logic Multi Player
+    function playGameMulti(socket) {
+        if(isgameover) return
+        if(!ready) {
+            socket.emit('player-ready')
+            ready = true
+            playerReady(playerNum)
+        }
+        if(enemyReady) {
+            if(currentPlayer === 'user') {
+                turnDisplay.innerHTML = 'Your Go'
+            }
+            if(currentPlayer === 'enemy') {
+                turnDisplay.innerHTML = 'Enemy Go'
+            }
+        }
+    }
+
+    function playerReady(num) {
+        let player = `.p${parseInt(num) + 1}`
+        document.querySelector(`${player} .ready span`).classList.toggle('green')
+    }
+
+    //Game logic Single Player
+    function playGameSingle() {
         if (isgameover) return
         if( currentPlayer === 'user') {
             turnDisplay.innerHTML = 'Your Go'
@@ -190,11 +313,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         if( currentPlayer === 'computer') {
             turnDisplay.innerHTML = 'Computers Go'
-            setTimeout(computerGo, 1000)
+            setTimeout(enemyGo, 1000)
         }
     }
 
-    startButton.addEventListener('click', playGame)
+    
 
     let destroyerCount = 0
     let submarineCount = 0
@@ -218,7 +341,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         checkForWins()
         currentPlayer = 'computer'
-        playGame()
+        playGameSingle()
     }
 
     let computerDestroyerCount = 0
@@ -227,16 +350,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let computerBattleshipCount = 0
     let computerCarrierCount = 0
 
-    function computerGo() {
-        let random = Math.floor(Math.random() * userSquares.length)
-        if(!userSquares[random].classList.contains('kapow')) {
-            userSquares[random].classList.add('kapow')
-            if(userSquares[random].classList.contains('destroyer')) computerDestroyerCount++
-            if(userSquares[random].classList.contains('submarine')) computerSubmarineCount++
-            if(userSquares[random].classList.contains('cruiser')) computerCruiserCount++
-            if(userSquares[random].classList.contains('battleship')) computerBattleshipCount++
-            if(userSquares[random].classList.contains('carrier')) computerCarrierCount++
-        } else computerGo()
+    function enemyGo(square) {
+        if(gameMode === 'singlePlayer') square = Math.floor(Math.square() * userSquares.length)
+        if(!userSquares[square].classList.contains('kapow')) {
+            userSquares[square].classList.add('kapow')
+            if(userSquares[square].classList.contains('destroyer')) computerDestroyerCount++
+            if(userSquares[square].classList.contains('submarine')) computerSubmarineCount++
+            if(userSquares[square].classList.contains('cruiser')) computerCruiserCount++
+            if(userSquares[square].classList.contains('battleship')) computerBattleshipCount++
+            if(userSquares[square].classList.contains('carrier')) computerCarrierCount++
+            checkForWins()
+        } else if(gameMode === 'singePlayer') enemyGo()
         currentPlayer = 'user'
         turnDisplay.innerHTML = 'Your Go'
     } 
@@ -297,6 +421,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function isGameOver() {
         isgameover = true
-        startButton.removeEventListener('click', playGame)
+        startButton.removeEventListener('click', playGameSingle)
     }
 })
--- server.js
+++ server.js
@@ -12,3 +12,70 @@ app.use(express.static(path.join(__dirname, "public")))
 
 //Start server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+//Handle socket connection rquest from io client
+const connections = [null, null]
+io.on('connection', socket => {
+    //console.log('New WS connection')
+
+    //Find available player nr
+    let playerIndex = -1
+    for(const i in connections) {
+        if(connections[i] === null) {
+            playerIndex = i
+            break
+        }
+    }
+
+    
+    //Tel connecting client what nr they are
+    socket.emit('player-number', playerIndex)
+    console.log(`Player ${playerIndex} has connected!`)
+
+    //Ignore player 3
+    if(playerIndex === -1) return
+
+    connections[playerIndex] = false
+
+    //What player just connected
+    socket.broadcast.emit('player-connection', playerIndex)
+
+    //Handle disconnecd
+    socket.on('disconnect', ()  => {
+        console.log(`Player ${playerIndex} disconnected`)
+        connections[playerIndex] = null
+        //Which player disconnected
+        socket.broadcast.emit('player-connection', playerIndex)
+    })
+
+    //On Ready
+    socket.on('player-ready', () => {
+        socket.broadcast.emit('enemy-ready', playerIndex)
+        connections[playerIndex] = true
+    })
+
+    //Check player connections
+    socket.on('check-players', () => {
+        const players = []
+        for(const i in connections) {
+            connections[i] === null ? players.push({connected: false, ready: false}) : players.push({connected: true, ready: connections[i]})
+        }
+        socket.emit('check-players', players)      
+    })
+
+    //On fire received
+    socket.on('fire', id => {
+        console.log(`Shot fired from ${playerIndex}`, id)
+
+        //Emit move to other player
+        socket.broadcast.emit('fire', id)
+    })
+
+    //On fire reply
+    socket.on('fire-reply', () => {
+        console.log(square)
+
+        //Forward reply to other player
+        socket.broadcast.emit('fire-reply', square)
+    })
+})
